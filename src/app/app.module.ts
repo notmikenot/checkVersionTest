0e217b50b28a5ea74899fc49b08dca6b5eb5f1e5
notmikenot <micheledragonetti99@gmail.com>
2020-05-06 11:10:13 +0200

    Miglioramenti

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VersionCheckService } from './core/version-check/version-check.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
     declarations: [
@@ -11,6 +12,7 @@ import { VersionCheckService } from './core/version-check/version-check.service'
     ],
     imports: [
         BrowserModule,
+        HttpClientModule,
         AppRoutingModule
     ],
     providers: [VersionCheckService],
